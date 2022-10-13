067febe07ba02253c26b19c86379c5dc63d3f831
Xuyang Kang <xuyangkang@gmail.com>
2022-10-13 21:17:50 +0100

    Update two-sum.cc

--- two-sum.cc
+++ two-sum.cc
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> last_seen;
-        vector<int> ans;
         for (int i = 0; i < nums.size(); i++) {
             int num = nums[i];
             auto it = last_seen.find(target - num);
