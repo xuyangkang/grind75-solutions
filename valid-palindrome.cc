e583fe37b54af810af2a7062c2a38084c9cacb9e
Xuyang Kang <xuyangkang@gmail.com>
2022-10-13 21:37:33 +0100

    Create valid-palindrome.cc

---
+++ valid-palindrome.cc
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    string cleanse(string s) {
+        string new_s;
+        for (char ch : s) {
+            if (isalnum(ch)) {
+                new_s.push_back(tolower(ch));
+            }
+        }
+        return new_s;
+    }
+public:
+    bool isPalindrome(string s) {
+        s = cleanse(s);
+        for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+            if (s[i] != s[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
