fcad155dc7c372c2e4e46cad1020cee41f00e1ae
Xuyang Kang <xuyangkang@gmail.com>
2022-10-13 21:17:13 +0100

    Update two-sum.cc

--- two-sum.cc
+++ two-sum.cc
@@ -3,17 +3,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> last_seen;
         vector<int> ans;
-        for (size_t i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) {
             int num = nums[i];
-            int diff = target - num;
             auto it = last_seen.find(target - num);
             if (it != last_seen.end()) {
-                ans.push_back(it->second);
-                ans.push_back(i);
-                break;
+                return {it->second, i};
             }
             last_seen[num] = i;
         }
-        return move(ans);
+        return {};
     }
 };
